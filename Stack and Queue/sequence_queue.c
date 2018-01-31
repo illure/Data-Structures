8384d911ecf4ae2834ffe0a0b115a0b0af1e8c86
illure <illure_work@outlook.com>
2018-01-31 16:36:32 +0800

    update

--- Stack and Queue/sequence_queue.c
+++ Stack and Queue/sequence_queue.c
@@ -11,3 +11,46 @@ typedef struct
 	int front, rear;
 } SqQueue;
 
+void InitQueue(SqQueue q);
+bool isEmpty(SqQueue q);
+bool EnQueue(SqQueue q, int x);
+bool DeQueue(SqQueue q, int x);
+
+void InitQueue(SqQueue q)
+{
+	q.front = q.rear = 0;
+}
+
+bool isEmpty(SqQueue q)
+{
+	if (q.front = q.rear)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool EnQueue(SqQueue q, int x)
+{
+	if ((q.rear + 1) % MaxSize == q.front)
+	{
+		return false;
+	}
+	q.data[q.rear] = x;
+	q.rear = (q.rear + 1) % MaxSize;
+	return true;
+}
+
+bool DeQueue(SqQueue q, int x)
+{
+	if (q.rear == q.front)
+	{
+		return false;
+	}
+	x = q.data[q.front];
+	q.front = (q.front + 1) % MaxSize;
+	return true;
+}
